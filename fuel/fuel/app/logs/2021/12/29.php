cd13d518af1ff6a52fe4c7e58259c8dd4a2dac16
Mohan <nahomnep@yahoo.com>
2021-12-30 11:19:23 +0900

    set up project

---
+++ fuel/fuel/app/logs/2021/12/29.php
@@ -0,0 +1,5 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2021-12-29 17:41:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-12-29 17:47:06 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-12-29 17:47:38 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
---
+++ fuel/fuel/app/logs/2021/12/30.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2021-12-30 10:02:01 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-12-30 11:04:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
